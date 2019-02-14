80ffc11682e2bcf5c6e8602afd8330c1db470e2b
Samuel Borges <samuelbfg@gmail.com>
2019-02-14 17:36:06 -0300

    Add all intented activities

--- Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/MainActivity.java
@@ -15,8 +15,10 @@
  */
 package com.example.android.miwok;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -26,5 +28,29 @@ public class MainActivity extends AppCompatActivity {
 
         // Set the content of the activity to use the activity_main.xml layout file
         setContentView(R.layout.activity_main);
+    }
+
+    // Opens NumbersActivity
+    public void openNumbersList(View view){
+        Intent i = new Intent(this, NumbersActivity.class);
+        startActivity(i);
+    }
+
+    // Opens FamilyActivity
+    public void openFamilyList(View view){
+        Intent i = new Intent(this, FamilyActivity.class);
+        startActivity(i);
+    }
+
+    // Opens ColorsActivity
+    public void openColorsList(View view){
+        Intent i = new Intent(this, ColorsActivity.class);
+        startActivity(i);
+    }
+
+    // Opens PhrasesActivity
+    public void openPhrasesList(View view0){
+        Intent i = new Intent(this, PhrasesActivity.class);
+        startActivity(i);
     }
 }
