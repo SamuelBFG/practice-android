8d8498bc4f6ce86b9a371a8fde02cfb5bf589765
Samuel Borges <samuelbfg@gmail.com>
2019-02-15 17:29:08 -0300

    NumbersActivity displaying the numbers w/ ArrayList

--- Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
+++ Miwok/app/src/main/java/com/example/android/miwok/NumbersActivity.java
@@ -2,6 +2,11 @@ package com.example.android.miwok;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.LinearLayout;
+import android.widget.TextView;
+
+import java.util.ArrayList;
 
 public class NumbersActivity extends AppCompatActivity {
 
@@ -9,5 +14,34 @@ public class NumbersActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_numbers);
+
+        // Creating words List
+        ArrayList<String> words = new ArrayList<String>();
+
+        // Populating the words List
+        words.add("one");
+        words.add("two");
+        words.add("three");
+        words.add("four");
+        words.add("five");
+        words.add("six");
+        words.add("seven");
+        words.add("eight");
+        words.add("nine");
+        words.add("ten");
+
+        // Find the LinearLayout rootView and store it in the variable rootView
+        LinearLayout rootView = (LinearLayout)findViewById(R.id.rootView);
+        // Populate the rootView with multiple TextView wordViews
+        for(int i = 0; i < words.size(); i++){
+            // Create the view wordView
+            TextView wordView = new TextView(this);
+            wordView.setText(words.get(i));
+            // Add the view as a child to the parent view rootView
+            rootView.addView(wordView);
+        }
+
+
+
     }
 }
